92915199ae0bfe81b00213e9df556a545105fbef
ilopX <ilopX@ukr.net>
2015-08-31 18:52:41 +0300

    Init

---
+++ README.md
@@ -0,0 +1,15 @@
+# jQuery detect change
+## Install
++ cdn
++ composer
++ bower
+
+## Use
++ include
++ detect change properties
++ detect change of condition
+
+## Global settings
++ speed update
++ name data selector
++ debug
---
+++ dist/jquery.detect-change.js
@@ -0,0 +1,106 @@
+/**
+ * Created by XTreme.ws on 31.08.2015.
+ */
+(function ( $ ) {
+    var DC = {
+        CONDITION: 0,
+        PROP: 1,
+        dataName: 'detect-change',
+        list: [],
+        allHook: function(func){
+            DC.list.forEach(function(select, i, arr){
+                select.data(DC.dataName).forEach(function(param, i, arr){
+                    func(select, param);
+                });
+            });
+        },
+        runChecker: function(select, param, init){
+            switch (param.type){
+                case DC.PROP:
+                    var currVal = select.prop(param.name);
+
+                    if (param.val != currVal){
+                        param.val = (param.val === undefined) ? null : param.val;
+                        param.func.call(select, param.val, currVal, init);
+                        param.val = currVal;
+                    }
+                    break;
+                case DC.CONDITION:
+                    var state = param.condition.call(select, init);
+                    param.func.call(select, param.val != state);
+                    param.val = state;
+
+                    break;
+            }
+        },
+        timer: function(){
+            DC.allHook(function(select, param){
+                DC.runChecker(select, param, false);
+            });
+        },
+
+        add: function(select, param){
+            // id selector add first time
+            if (!select.data(DC.dataName)){
+                select.data(DC.dataName, []);
+                DC.list.push(select);
+            }
+
+            DC.runChecker(select, param, true);
+
+            select.data(DC.dataName).push(param);
+            //select.prop('data', select.data(DC.dataName)); // debug info
+            DC.timer();
+        },
+
+        remove: function remove(select, name){
+
+        }
+    };
+
+    DC.timerID = setInterval(DC.timer, 100);
+
+
+    $.fn.detectChange = function(name, func, condition) {
+        var param = {
+            name: name,
+            func: func,
+            condition: condition
+        };
+
+        // remove hook
+        if (func === 'remove'){
+            DC.remove(this, name);
+        }
+        // add hook, selector properties or user condition
+        else{
+            param.type = (typeof condition === 'function') ? DC.CONDITION : DC.PROP;
+            DC.add(this, param);
+        }
+
+        return this;
+    };
+}( jQuery ));
+// TODO: create separate project and put to git repository
+// TODO: create examples
+// TODO: add function remove
+// TODO: add self destroy if selector remove
+// TODO: add global setting(speed, name data)
+// TODO: add composer and bower
+$('body')
+    // selector properties
+    .detectChange('clientWidth', function(oldVal, newVal, init){
+        console.log(this[0].clientWidth == newVal);
+    })
+    // user condition
+    .detectChange('your-name',
+        function(change){
+            if (!change)
+                return;
+           console.log('больше 600')
+        },
+        function(init){
+            return this.width() > 600;
+        });
+
+
